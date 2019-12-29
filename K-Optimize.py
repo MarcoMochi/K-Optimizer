260ab1551bb619f979914025e218ea1da9c968b5
MarcoMochi <Marcomochi95@gmail.com>
2019-12-29 09:59:12 +0100

    Added cost function

--- K-Optimize.py
+++ K-Optimize.py
@@ -6,12 +6,12 @@ k = 5
 tupleList = [x for x in range(0,500)]
 rangeDict = {}
 equivalenceClassDict = {}
-tempEquivalenceClassDict = {}
 for row in rangeDb.itertuples(index=True, name='Pandas'):
     rangeDict[getattr(row, "Index")] = getattr(row, "Value")
 H = [2, 12, 22]
 stopValue = [2, 12, 22, 28]
 T = [i for i in rangeDict.keys() if i not in H]
+T.remove(0)
 equivalenceClassDict["2.12.22"] = tupleList
 
 def MaxOfSmaller(values, n):
@@ -21,13 +21,14 @@ def MinOfGreater(values, n):
     return min(m for m in values if m > n)
 
 def EquivalenceClass(H, save = True):
-    tempEquivalenceClassDict = equivalenceClassDict
+    tempEquivalenceClassDict = equivalenceClassDict.copy()
     for v in H:
         key1 = key2 = ""
         if not any(str(v) in i.split(".") for i in equivalenceClassDict.keys()):
             for k in list(equivalenceClassDict.keys()):
                 key1 = key2 = ""
                 valueSplit = [int(n) for n in k.split(".")]
+                valueSplit.sort()
                 if v < valueSplit[0]:
                     continue
                 else:
@@ -39,9 +40,11 @@ def EquivalenceClass(H, save = True):
                 if minSplit >= MaxOfSmaller(stopValue, v):
                     secondStep = valueSplit[valueSplit.index(minSplit)-1]
                     if (secondStep < MaxOfSmaller(stopValue,v) and maxSplit <= MinOfGreater(stopValue, v)) or minSplit == stopValue[0]:
+                        tupleList = equivalenceClassDict[k]
                         if save:
-                            tupleList = equivalenceClassDict[k]
                             del equivalenceClassDict[k]
+                        else:
+                            del tempEquivalenceClassDict[k]
                         for m in valueSplit:
                             if m == minSplit:
                                 key1 = key1 + str(m) + "."
@@ -56,17 +59,20 @@ def EquivalenceClass(H, save = True):
                                 key2 = key2 + str(m) + "."
                         key1 = key1[:-1]
                         key2 = key2[:-1]
-                        if key1 != "" and save:
+                        if key1 != "":
                             columnIndex = stopValue.index(MinOfGreater(stopValue,v))
                             if maxSplit == 0:
                                 maxSplit = 28
                             elif maxSplit > MinOfGreater(stopValue, v):
                                 maxSplit = MinOfGreater(stopValue, v)
-                            equivalenceClassDict[key1] = CreateRange(columnIndex, minSplit, v, tupleList)
-                            equivalenceClassDict[key2] = CreateRange(columnIndex, v,maxSplit, tupleList)
-                        elif not save:
-                            tempEquivalenceClassDict[key1] = ""
-                            tempEquivalenceClassDict[key2] = ""
+                            if save:
+                                equivalenceClassDict[key1] = CreateRange(columnIndex, minSplit, v, tupleList)
+                                equivalenceClassDict[key2] = CreateRange(columnIndex, v,maxSplit, tupleList)
+                            else:
+                                tempEquivalenceClassDict[key1] = CreateRange(columnIndex, minSplit, v, tupleList)
+                                tempEquivalenceClassDict[key2] = CreateRange(columnIndex, v,maxSplit, tupleList)
+    if not save:
+        return tempEquivalenceClassDict
 
 def CreateRange(columnIndex, start, end, tupleList):
     tempList = []
@@ -86,18 +92,32 @@ def CreateRange(columnIndex, start, end, tupleList):
 
 def PruneUselessValue(H, T):
      for v in T:
-         tempList = H + v
-         EquivalenceClass(H, save=False)
-         sizes = [len(v) for v in tempEquivalenceClassDict.values()]
-         if all(sizes < k):
+         tempList = H + [v]
+         temp = EquivalenceClass(tempList, save=False)
+         sizes = [len(v) for v in temp.values()]
+         print(sizes)
+         if all(size < k for size in sizes):
+             print("Rimuoviamo " + str(v))
              T.remove(v)
+     return T
 
+def ComputeCost():
+    cost = 0
+    for item in equivalenceClassDict.values():
+        if len(item) >= k:
+            cost += (len(item))**2
+        else:
+            cost+= len(item) * len(tupleList)
+    return cost
+
+ComputeCost()
 EquivalenceClass([2,12,15,22])
-EquivalenceClass([2,12,15,18,22])
-EquivalenceClass([2,12,15,18,22,25])
-EquivalenceClass([2,12,14,15,18,22,25,27])
+ComputeCost()
 EquivalenceClass([2,5,12,15,22])
+ComputeCost()
+# EquivalenceClass([2,12,15,18,22])
+# EquivalenceClass([2,12,15,18,22,25])
+# EquivalenceClass([2,12,14,15,18,22,25,27])
+# EquivalenceClass([2,5,12,15,22])
+#PruneUselessValue(H,T)
 
-print(equivalenceClassDict)
-for item in equivalenceClassDict:
-    print(len(equivalenceClassDict[item]))

ef355e5b40b5980c976bf58b285638498941aad6
MarcoMochi <Marcomochi95@gmail.com>
2020-01-14 14:36:30 +0100

    Add gender

--- K-Optimize.py
+++ K-Optimize.py
@@ -4,26 +4,23 @@ import pandas as pd
 
 dataset = pd.read_csv("./Dataset/BMI.csv")
 rangeDb = pd.read_csv("./Dataset/rangeValue.csv")
-k = 15
+k = 5
 sup = math.inf
 iteration = 0
 tupleList = [x for x in range(0,500)]
 rangeDict = {}
 for row in rangeDb.itertuples(index=True, name='Pandas'):
     rangeDict[getattr(row, "Index")] = getattr(row, "Value")
-H = [2, 12, 22]
-stopValue = [2, 12, 22, 28]
+H = [0, 2, 12, 22]
+stopValue = [0, 2, 12, 22, 28]
 T = [i for i in rangeDict.keys() if i not in H]
-T.remove(0)
-T.remove(1)
+T.remove(28)
 bestCost = []
 savedClass = {}
-
 
 def MaxOfSmaller(values, n):
     """ Takes an array values and a value n, it gives the biggest number smaller than n """
     return max(m for m in values if m < n)
-
 def MinOfGreater(values, n):
     """ Takes an array values and a value n, it gives the biggest number smaller than n """
     return min(m for m in values if m > n)
@@ -36,7 +33,7 @@ def EquivalenceClass(H, equivalenceClassDict, save = True):
     for v in H:
         key1 = key2 = ""
         if not any(str(v) in i.split(".") for i in tempEquivalenceClassDict.keys()): # check if a value is new for this H
-            for k in list(tempEquivalenceClassDict.keys()):
+           for k in list(tempEquivalenceClassDict.keys()):
                 key1 = key2 = ""
                 valueSplit = [int(n) for n in k.split(".")]
                 valueSplit.sort()
@@ -63,7 +60,7 @@ def EquivalenceClass(H, equivalenceClassDict, save = True):
                                 key2 = key2 + str(v) + "."
                             elif m == maxSplit:
                                 key2 = key2 + str(m) + "."
-                                if m >= MinOfGreater(stopValue, v):
+                                if m != 0 and m >= MinOfGreater(stopValue, v):
                                     key1 = key1 + str(m) + "."
                             else:
                                 key1 = key1 + str(m) + "."
@@ -71,9 +68,9 @@ def EquivalenceClass(H, equivalenceClassDict, save = True):
                         key1 = key1[:-1]
                         key2 = key2[:-1]
                         if key1 != "":
-                            columnIndex = stopValue.index(MinOfGreater(stopValue,v))
+                            columnIndex = stopValue.index(MinOfGreater(stopValue,v)) - 1
                             if maxSplit == 0:
-                                maxSplit = 28
+                                maxSplit = stopValue[-1]
                             elif maxSplit > MinOfGreater(stopValue, v):
                                 maxSplit = MinOfGreater(stopValue, v)
                             if save:
@@ -94,14 +91,14 @@ def CreateRange(columnIndex, start, end, tupleList):
         Return the tuples of this new class
     """
     tempList = []
-    startRange = int(rangeDb.iloc[start-1, 1])
+    startRange = int(rangeDb.iloc[start, 1])
     if end in stopValue:
         for index in tupleList:
             checkValue = int(dataset.iloc[index, columnIndex])
             if checkValue >= startRange:
                 tempList.append(index)
     else:
-        endRange = int(rangeDb.iloc[end-1, 1])
+        endRange = int(rangeDb.iloc[end, 1])
         for index in tupleList:
             checkValue = int(dataset.iloc[index, columnIndex])
             if checkValue >= startRange and checkValue < endRange:
@@ -185,7 +182,7 @@ def Prune(H, T, c, eCD):
     """ Try to delete a node with H and T. if it can't it will try to delete values from T.
         After deleting a value from T it retry to delete H
     """
-    if ComputeLBCost(H,T, eCD) >= c:
+    if check == 1 and ComputeLBCost(H,T, eCD) >= c:
         return []
     for v in T:
         newH = H + [v]
@@ -196,9 +193,9 @@ def Prune(H, T, c, eCD):
             if ComputeLBCost(H, T, eCD) >= c:
                 return []
         else:
-            newH.sort()
             if Suppression(savedClass[str(newH)]) > sup:
                 T.remove(v)
+
     return T
 
 def Suppression(eCD):
@@ -207,26 +204,45 @@ def Suppression(eCD):
     return tot
 
 def AnonymizeDataset(eCD):
-    finalDb = []
-    for key, values in eCD.items():
-        print(len(values))
-        for index in values:
-            finalDb.append((key, values))
+    final = []
+
+    for key in eCD.keys():
+        keySplit = [int(n) for n in key.split(".")]
+        temp = []
+        while keySplit:
+            a = keySplit.pop(0)
+            a_value = int(rangeDb.iloc[a, 1])
+            try:
+                keySplit[0]
+                if keySplit[0] < MinOfGreater(stopValue, a):
+                    b = keySplit.pop(0)
+                    b_value = int(rangeDb.iloc[b, 1])
+                    temp.append(str(a_value) + "-" + str(b_value))
+                else:
+                    temp.append(str(a_value) + "->")
+            except:
+                temp.append(str(a_value) + "->")
+        for i in range(len(eCD[key])):
+            final.append(temp)
+
+    finalDb = pd.DataFrame(final, columns=['Gender', 'Height', 'Weight', 'Index'])
     return finalDb
 
 def K_Optimize(H, T, c, eCD):
     global iteration
     print("Iterazione numero :" + str(iteration))
     iteration += 1
-    T = PruneUselessValue(H, T, eCD.copy())
-    c = min(c, ComputeCost(eCD.copy()))
+    T = PruneUselessValue(H, T, eCD)
+    c = min(c, ComputeCost(eCD))
     if c < bestCost[0][1]:
+        global check
+        check = 1
         bestCost.clear()
         bestCost.append((H, c,  eCD.copy()))
         print("best anonymization found!")
         print(str(H) + "with cost:" + str(c) + " iteration:" + str(iteration))
-    T = Prune(H, T, c, eCD.copy())
-    T = ReorderTail(H, T, eCD.copy())
+    T = Prune(H, T, c, eCD)
+    T = ReorderTail(H, T, eCD)
     while T:
         v = T[0]
         newH = H + [v]
@@ -235,15 +251,32 @@ def K_Optimize(H, T, c, eCD):
         try:
             savedClass[str(newH)]
         except:
-            tempeCD = EquivalenceClass(newH, savedClass[str(H)])
-            savedClass[str(newH)] =tempeCD.copy()
-        c = K_Optimize(newH, T, c, savedClass[str(newH)].copy())
-        T = Prune(H, T, c, savedClass[str(newH)].copy())
+            savedClass[str(newH)] = EquivalenceClass(newH, savedClass[str(H)]).copy()
+        c = K_Optimize(newH, T, c, savedClass[str(newH)])
+        T = Prune(H, T, c, savedClass[str(newH)])
     return c
 
+
 equivalenceClassDict = {}
-equivalenceClassDict["2.12.22"] = tupleList
-bestCost.append((H, math.inf))
-K_Optimize(H, T, math.inf, equivalenceClassDict.copy())
+equivalenceClassDict["0.2.12.22"] = tupleList
+k = input("Inserisci il valore di k:\n")
+k = int(k)
+sup = input("Inserisci il numero massimo di soppressioni permesse, -1 per non avere limiti:\n")
+sup = int(sup)
+seed = input("Inserisci un valore di seed, 0 per non inserirne:\n")
+seed = int(seed)
+path = input("Inserisci il nome del file in cui vuoi stampare il risultato, 0 per non salvare il file:\n")
+path = str(path)
+check = 0
+if seed == 0:
+    seed = math.inf
+    check = 1
+if sup == -1:
+    sup = math.inf
+bestCost.append((H, seed))
+K_Optimize(H, T, seed, equivalenceClassDict.copy())
 final = AnonymizeDataset(bestCost[0][2])
-print(final)
+if path == "0":
+    print(final)
+else:
+    final.to_csv(path_or_buf=("./" + str(path)))
